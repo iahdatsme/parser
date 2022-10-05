c65add00c4f3ae1be3b0f9bc0ba880f52caa626f
iahdatsme <55593645+iahdatsme@users.noreply.github.com>
2022-10-05 14:59:38 -0500

    Add files via upload

--- parser.rkt
+++ parser.rkt
@@ -11,22 +11,18 @@
     [(char-alphabetic? (first i)) (cons 'ID (scanner (rest i)))]
     [(or (equal? (first i) #\space) (equal? (first i) #\return) (equal? (first i) #\newline)) (scanner (rest i))]
     [(and (equal? (first i) #\:) (equal? (second i) #\=)) (cons 'Eq (scanner (rest (rest i))))]
-    [(equal? (first (list i)) #\() (cons 'Lparen (scanner (rest i)))]
-    [(equal? (first (list i)) #\)) (cons 'Rparen (scanner (rest i)))]
-    [(equal? (first (list i)) #\+) (cons 'Plus (scanner (rest i)))]
-    [(equal? (first (list i)) #\*) (cons 'Multiply (scanner (rest i)))]
-    [(equal? (first (list i)) #\-) (cons 'Minus (scanner (rest i)))]
-    [(equal? (first (list i)) #\/) (cons 'Divide (scanner (rest i)))]
+    [(equal? (first i) #\() (cons 'Lparen (scanner (rest i)))]
+    [(equal? (first i) #\)) (cons 'Rparen (scanner (rest i)))]
+    [(equal? (first i) #\+) (cons 'Plus (scanner (rest i)))]
+    [(equal? (first i) #\*) (cons 'Multiply (scanner (rest i)))]
+    [(equal? (first i) #\-) (cons 'Minus (scanner (rest i)))]
+    [(equal? (first i) #\/) (cons 'Divide (scanner (rest i)))]
     
 
     [else "Scanner Error" (rest i)]
     )
   
   )
-
-
-;(define parser)
-
 
 (scanner input)
 (trace scanner)
