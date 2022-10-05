952768c98a9bc5590aa857f88fd2cfdf422a27a0
iahdatsme <55593645+iahdatsme@users.noreply.github.com>
2022-10-05 14:24:20 -0500

    Add files via upload

--- parser.rkt
+++ parser.rkt
@@ -4,24 +4,29 @@
 ;; SCANNER
 (define i (string->list (file->string "input01.txt")))
 
-(define (scanner i) ;
+(define (scanner i)
   (cond
-    [(or(empty? i) (equal? (first i) #\$))]
-    [(or (equal? (first i) #\space) (equal? (first i) #\return) (equal? (first i) #\newline)) (scanner (first(rest i)))]
-    [(and (equal? (first i) #\:) (first i) #\=) (cons 'Eq (scanner (rest i)))]
-    [(equal? (first i) #\()(rest(first i)) (cons 'Lparen (scanner (rest i)))]
-    [(equal? (first i) #\))(rest(first i)) (cons 'Rparen (scanner (rest i)))]
-    [(equal? (first i) #\+)(rest(first i)) (cons 'Plus (scanner (rest i)))]
-    [(equal? (first i) #\*) (rest(first i)) (cons 'Multiply (scanner (rest i)))]
-    [(equal? (first i) #\-)(rest(first i)) (cons 'Minus (scanner (rest i)))]
-    [(equal? (first i) #\/) (rest(first i)) (cons 'Divide (scanner (rest i)))]
-    [(equal? (first i) char-numeric?) (rest(first i)) (cons 'Num (scanner (rest i)))]
-    [(equal? (first i) char?) (rest(first i)) (cons 'ID (scanner (rest i)))]
+    [(or(empty? i) (equal? (first (list i)) #\$))]
+    [(char-numeric? (first i)) (rest(first (list i))) (cons 'Num (scanner (rest i)))]
+    [(char-alphabetic? (first i)) (rest(first (list i))) (cons 'ID (scanner (rest i)))]
+    [(or (equal? (first (list i)) #\space) (equal? (first (list i)) #\return) (equal? (first (list i)) #\newline)) (scanner (first(rest i)))]
+    [(and (equal? (first (list i)) #\:) (equal? (first (list i)) #\=)) (cons 'Eq (scanner (rest (rest i))))]
+    [(equal? (first (list i)) #\() (rest(first  (list i))) (cons 'Lparen (scanner (rest i)))]
+    [(equal? (first (list i)) #\))(rest(first  (list i))) (cons 'Rparen (scanner (rest i)))]
+    [(equal? (first (list i)) #\+)(rest(first  (list i))) (cons 'Plus (scanner (rest i)))]
+    [(equal? (first (list i)) #\*) (rest(first  (list i))) (cons 'Multiply (scanner (rest i)))]
+    [(equal? (first (list i)) #\-)(rest(first  (list i))) (cons 'Minus (scanner (rest i)))]
+    [(equal? (first (list i)) #\/) (rest(first (list i))) (cons 'Divide (scanner (rest i)))]
+    
 
     [else "Scanner Error" (rest i)]
     )
   
   )
+
+
+;(define parser)
+
 
 (scanner i)
 (trace scanner)
