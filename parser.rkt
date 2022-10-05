e54be954f6502ce434b8de536df7008788cb9e25
iahdatsme <55593645+iahdatsme@users.noreply.github.com>
2022-10-05 14:49:33 -0500

    Add files via upload

--- parser.rkt
+++ parser.rkt
@@ -6,17 +6,17 @@
 
 (define (scanner i)
   (cond
-    [(or(empty? i) (equal? (first (list i)) #\$)) (scanner (rest i))]
-    [(char-numeric? (first i)) (rest(first (list i))) (cons 'Num (scanner (rest i)))]
-    [(char-alphabetic? (first i)) (rest(first (list i))) (cons 'ID (scanner (rest i)))]
-    [(or (equal? (first i) #\space) (equal? (first i) #\return) (equal? (first i) #\newline)) (scanner (first(rest i)))]
-    [(and (equal? (first (list i)) #\:) (equal? (first i) #\=)) (cons 'Eq (scanner (rest (rest i))))]
-    [(equal? (first (list i)) #\() (rest(first i)) (cons 'Lparen (scanner (rest i)))]
-    [(equal? (first (list i)) #\))(rest(first i)) (cons 'Rparen (scanner (rest i)))]
-    [(equal? (first (list i)) #\+)(rest(first i)) (cons 'Plus (scanner (rest i)))]
-    [(equal? (first (list i)) #\*) (rest(first i)) (cons 'Multiply (scanner (rest i)))]
-    [(equal? (first (list i)) #\-)(rest(first i)) (cons 'Minus (scanner (rest i)))]
-    [(equal? (first (list i)) #\/) (rest(first i)) (cons 'Divide (scanner (rest i)))]
+    [(or(empty? i) (equal? (first (list i)) #\$)) (list 'eof)]
+    [(char-numeric? (first i)) (cons 'Num (scanner (rest i)))]
+    [(char-alphabetic? (first i)) (cons 'ID (scanner (rest i)))]
+    [(or (equal? (first i) #\space) (equal? (first i) #\return) (equal? (first i) #\newline)) (scanner (rest i))]
+    [(and (equal? (first i) #\:) (equal? (second i) #\=)) (cons 'Eq (scanner (rest (rest i))))]
+    [(equal? (first (list i)) #\() (cons 'Lparen (scanner (rest i)))]
+    [(equal? (first (list i)) #\)) (cons 'Rparen (scanner (rest i)))]
+    [(equal? (first (list i)) #\+) (cons 'Plus (scanner (rest i)))]
+    [(equal? (first (list i)) #\*) (cons 'Multiply (scanner (rest i)))]
+    [(equal? (first (list i)) #\-) (cons 'Minus (scanner (rest i)))]
+    [(equal? (first (list i)) #\/) (cons 'Divide (scanner (rest i)))]
     
 
     [else "Scanner Error" (rest i)]
